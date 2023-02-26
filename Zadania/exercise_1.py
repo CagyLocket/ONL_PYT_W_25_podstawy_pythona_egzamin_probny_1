7cf28434909e6b2051fee738ac102b0557c64b99
pawelwrzesien <pawel.wrzesien@icloud.com>
2023-02-26 13:25:30 +0100

    Exercise 1 done.

--- Zadania/exercise_1.py
+++ Zadania/exercise_1.py
@@ -1,2 +1,23 @@
-def shorten():
-    pass
+def shorten(input_string):
+    """Splits a long string by space and joins capitalized first letters from each string item together .
+
+    :param str input_string: ex. 'Don't repeat yourself'
+    :rtype: str
+    :return: string as abbreviation from capitalized first letters of each string item"""
+
+    output_string = ''.join([x[:1].upper() for x in input_string.split(' ')])
+
+    return output_string
+
+
+shortened = shorten("Don't repeat yourself")
+print(shortened)
+
+shortened = shorten("Read the fine manual")
+print(shortened)
+
+shortened = shorten("All terrain armoured transport")
+print(shortened)
+
+
+
